a8af72979b82a0c773f1792ffe1c2035087ff826
SagarPalo24 <spalo242526@gmail.com>
2024-06-07 21:27:02 +0530

    useEffect and search bar

--- src/components/Cardcontainor.js
+++ src/components/Cardcontainor.js
@@ -1,37 +1,52 @@
 import Restaurantcard from "./Restaurantcard";
-import {restaurantList} from "../const/config";
 import {IMG_URL} from "../const/config";
 import {useState,useEffect} from "react";
 
 const Cardcontainor =() =>{
-    const [restaurantData,setRestaurantData] =useState(restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-console.log ("restaurantList",restaurantList)
+    const [restaurantData,setRestaurantData] =useState([]);
+    const [searchText,setSearchText]=useState("");
 
-// setRestaurantData (restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-
-const filterRestaurants =()=>{
-    const restaurants= restaurantData.filter((restaurant)=>{
-        return(
-            restaurant?.info?.avgRating>= 4.5
-            )
-    })
-    setRestaurantData(restaurants);
+const getRestaurats = async()=>{
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json=await data.json();
+    console.log ("json",json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setRestaurantData(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
 }
 
-return(
-<div className="container grid grid-cols-5">
-    <button onClick={filterRestaurants}> Top rated restaurant</button>
-    {
-    restaurantData.map((restaurant) =>{
-    return(
-    <Restaurantcard
-    key={restaurant?.info.id}
-    {...restaurant?.info}
-    />
-    )
+const handleSearchText=(event)=>{
+    setSearchText(event.target.value)
+}
+
+const filterData=()=>{
+    const filterData = restaurantData.filter((restaurant)=>{
+        return restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase())
     })
-    }
-</div>
+    setRestaurantData(filterData);
+}
+
+useEffect(()=>{
+    getRestaurats();
+},[])
+
+return(
+    <div>
+        <div className="container">
+             <input type="text" className="custom_input" placeholder="Enter Name of Restaurant" value={searchText} onChange={handleSearchText}/>
+             <button onClick={filterData} className="btn btn-sm btn-light">🔍</button>
+        </div>
+        <div className="container grid grid-cols-5">
+            {
+                restaurantData.map((restaurant) =>{
+                return(
+                <Restaurantcard
+                key={restaurant?.info.id}
+                {...restaurant?.info}
+                />
+                )
+                })
+            }
+        </div>
+    </div>
 );
 };
 export default Cardcontainor;
